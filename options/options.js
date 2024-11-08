3fac31dd589842021ebef1dfdd9e21dc16f01755
Crowdbotics <team@crowdbotics.com>
2024-11-08 17:11:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-union-49951",
+  url: "https://divine-union-49951.botics.co",
+  api: "https://divine-union-49951.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
